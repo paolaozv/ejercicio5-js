c52da0f75e00578103e673cecadec7fd693fa162
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-08-25 19:33:57 -0500

    ejercicio 5

---
+++ app/app.js
@@ -0,0 +1,9 @@
+var numero = prompt("Ingresa un número");
+    numero = parseInt(numero);
+   
+   if (numero >= 0) {
+   		document.write("Número positivo");
+   }
+   else {
+   		document.write("Número negativo");
+   }
